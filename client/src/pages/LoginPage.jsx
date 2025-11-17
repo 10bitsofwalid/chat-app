9475d105e357aef9264d547638bf69b1cc03d1bc
Walid <walidrahman.officials@gmail.com>
2025-11-17 20:08:37 +0600

    login page file updated

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -44,6 +44,14 @@ const LoginPage = () => {
                     <p>Agree to the terms of the use & privacy policy.</p>
                 </div>
 
+                <div className='flex flex-col gap-2'>
+                    {currState === "Sign up" ? (
+                        <p className='text-sm text-gray-600'>Already have an account? <span className='font-medium text-violet-500 cursor-pointer'>Login here </span></p>
+                    ): (
+                        <p className='text-sm text-gray-600'>Create an account <span className='font-medium text-violet-500 cursor-pointer'>Click here</span></p>
+                    )}
+                </div>
+
         </form>
 
     </div>
