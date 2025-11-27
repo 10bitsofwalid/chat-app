3844e0d160038a757fc04fcf9bd0258c2ab8086d
Walid <walidrahman.officials@gmail.com>
2025-11-27 23:20:40 +0600

    files updated:

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 
 
@@ -19,6 +20,7 @@ app.use(cors());
 //route setup
 app.use("/api/status", (req, res)=> res.send("server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 //connect to db
 await connectDB();
