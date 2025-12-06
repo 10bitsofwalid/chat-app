2f330116b280ef6438689a892bebd8cadc865b08
Walid <walidrahman.officials@gmail.com>
2025-12-06 22:35:33 +0600

    files updated

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import assets, { imagesDummyData } from '../assets/assets'
+import { ChatContext } from '../../context/ChatContext'
+import { AuthContext } from '../../context/AuthContext'
 
-const RightSidebar = ({selectedUser}) => {
+const RightSidebar = () => {
+    
+    const {selectedUser, messages} = useContext(ChatContext)
+    const {logout, onlineUsers} = useContext(AuthContext)
+    const [msgImages, setMsgImages] = useState([])
+
     return selectedUser && (
     <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : ""}`}>
         <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
