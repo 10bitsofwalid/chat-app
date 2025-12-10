ad63fea62367dc46088e2a46c802c79811e8f3ec
Walid <walidrahman.officials@gmail.com>
2025-12-10 21:34:40 +0600

    files updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const { authUser } = useContext(AuthContext)
 
   return(
-    <div className= "bg-[url('./src/assets/bgNew14.jpg')]">
+    <div className= "bg-[url('./src/assets/bgNew8.jpg')]">
       <Toaster />
       <Routes>
         <Route path='/' element={ authUser ? <HomePage /> : <Navigate to="/login" />} />
